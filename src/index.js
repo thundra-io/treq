27d40608ffce2dc292eb37ae2e5c741e8be5da0d
Serkan ÖZAL <serkan@thundra.io>
2022-03-28 23:48:16 +0300

    Initial impl

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const Module = require('module');
+const originalRequire = Module.prototype.require;
+
+const DURATION_THRESHOLD =
+    parseInt(process.env['TREQ_DURATION_THRESHOLD']) || 10;
+
+class RequireTrace {
+    constructor(moduleId) {
+        this.moduleId = moduleId;
+        this.duration = 0;
+        this.children = [];
+    }
+}
+
+const activeReqs = [];
+
+Module.prototype.require = function traceRequires(id) {
+    // Get current time
+    const ts = process.hrtime();
+
+    // Create new require trace
+    const reqTrace = new RequireTrace(id);
+    const numOfActiveReqs = activeReqs.length;
+    // Get parent require trace
+    const parentReqTrace =
+        numOfActiveReqs === 0 ? null : activeReqs[numOfActiveReqs - 1];
+    if (parentReqTrace) {
+        // Link current require trace as child of parent require trace
+        parentReqTrace.children.push(reqTrace);
+    }
+
+    let res;
+    // Push new require traces onto stack as current require trace
+    activeReqs.push(reqTrace);
+    try {
+        res = originalRequire.call(this, id);
+    } finally {
+        // Pop new require trace from stack as it is not current require trace anymore
+        activeReqs.pop();
+    }
+
+    // Calculate passed time
+    const passedTime = process.hrtime(ts);
+    // Set duration of the current require trace
+    reqTrace.duration = passedTime[0] * 1000 + passedTime[1] / 1e6;
+
+    // If current require trace is the root trace
+    if (numOfActiveReqs === 0) {
+        // Dump it with its children
+        dumpRequireStack(reqTrace);
+    }
+
+    return res;
+};
+
+function dumpRequireStack(reqTrace, depth = 0) {
+    if (reqTrace.duration && reqTrace.duration >= DURATION_THRESHOLD) {
+        let indent = '';
+        for (let i = 0; i < depth; i++) {
+            indent += '..';
+        }
+        console.log(
+            "%s|-- require('%s') took %s ms",
+            indent,
+            reqTrace.moduleId,
+            reqTrace.duration
+        );
+        for (let childReqTrace of reqTrace.children) {
+            dumpRequireStack(childReqTrace, depth + 1);
+        }
+    }
+}
